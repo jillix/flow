e3bb6c7bb0eccd3ba9068b0d12bcdca672eabf7b
Adrian Ottiker <adrian@ottiker.com>
2016-04-02 23:29:52 +0000

    renamed to "data.js"; former "basic.js"

--- test/basic.js
+++
@@ -1,7 +0,0 @@
-var test = require('./app/test');
-var tap = require('tap');
-
-//tap.test('Data handlers only:', test('test/data'));
-//tap.test('Emit:', test('test/end'));
-tap.test('Node:', test('test/stream'));
-//tap.test('Stream handler only:', test('test/stream'));
