eec47960a4cf439d572271ed62d12df335f47cc3
Adrian Ottiker <adrian@ottiker.com>
2016-09-28 20:36:14 +0200

    remodel code architecture

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
-const Node = require('./lib/Node');
-const Loader = require('./lib/Loader');
+const Stream = require('./lib/Stream');
 
 // factory
 module.exports = (adapter) => {
@@ -30,40 +29,6 @@ module.exports = (adapter) => {
     }; 
 
     return (event, args) => {
-        return Flow(scope, event, args)
+        return Stream(scope, event, args)
     };
 };
-
-function Flow (scope, event, args = {}) {
-
-    // return if event name is missing
-    if (!event) {
-        throw new Error('Flow: Empty event.');
-    }
-
-    // return cached streams
-    // TODO args for cached nodes??
-    // TODO check if it's better, if the modules handles
-    //      the reuse of streams?
-    let node = scope.cache.get('s:' + event);
-    if (node) {
-        return node;
-    }
-
-    node = Node(scope, event, args);
-
-    // handle cached event
-    let parsed_event = scope.cache.get('l:' + event);
-    if (parsed_event) { 
-        process.nextTick(() => node.link(args, parsed_event));
-
-    // pipe triple stream to loader
-    } else {
-        scope.read(event, args).pipe(Loader(scope, node, args));
-    }
-
-    // save stream in cache
-    scope.cache.set('s:' + event, node);
-
-    return node;
-}
