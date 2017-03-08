62ed3a064bfd2be37a751977b1153c1ca1f86b63
Adrian Ottiker <adrian@ottiker.com>
2017-03-09 00:05:39 +0100

    sequence arguments and optimizations

--- index.js
+++ index.js
@@ -7,41 +7,23 @@ const Emit = require("./lib/Emit");
 const Event = require("./lib/Event");
 const Sequence = require("./lib/Sequence");
 
-module.exports = (env = {}, adapter) => {
+module.exports = (adapter) => {
 
     if (!adapter.cache || !adapter.seq || !adapter.fn) {
         throw new Error("Flow: Invalid adapter.");
     }
 
-    const scope = {
-        env: env,
-        fn: adapter.fn,
-        seq: adapter.seq,
-        cache: adapter.cache,
-        reset: () => {
+    function call (options, data, done) {
 
-            if (typeof adapter.reset === "function") {
-                adapter.reset();
-            }
+        const event = Event(options, done);
 
-            adapter.cache.reset();
-        }
-    };
-
-    return scope.flow = (sequence_id, data, options, done) => {
-
-        if (typeof options === "function") {
-            done = options;
-            options = {};
-        }
-
-        const event = Event(scope, sequence_id, data, options, done);
-
-        Sequence(scope, sequence_id, event)
+        Sequence(adapter, call, event, data)
         .then(Emit)
         .then(event.open)
         .catch(event.done);
 
         return event;
     };
+
+    return call;
 };
