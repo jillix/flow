33ba0af3e6c170f8d1a3ef32659c09dd589f66ec
Adrian Ottiker <adrian@ottiker.com>
2017-06-11 12:51:32 +0200

    fix error propagation, fix sequence promise buildup

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 "use strict"
 
 module.exports = (adapter) => {
-    const call = (options, data, done) => {
+    const call = (event, input) => {
 
-        const flow = {
-            emit: call,
-            sequence: options.sequence,
-            role: options.role || null
-        };
+        event = typeof event === "string" ? event = {sequence: event} : event;
+        event.emit = call;
 
-        let parsed_sequence = adapter.cache.get(options.sequence);
+        let parsed_sequence = adapter.cache.get(event.sequence);
         if (!parsed_sequence) {
-            parsed_sequence = adapter.seq(options.sequence, options.role)
-            .then(checkRoleAccess(options.role))
-            .then(parse(adapter))
+            parsed_sequence = adapter.seq(event.sequence, event.role)
+            .then(checkRoleAccess(event.role))
+            .then(parse(adapter, event.role))
             .then((parsed) => {
-                adapter.cache.set(options.sequence, parsed);
+                adapter.cache.set(event.sequence, parsed);
                 return parsed;
             });
         } else {
-            parsed_sequence = Promise.resolve(parsed_sequence).then(checkRoleAccess(options.role));
+            parsed_sequence = Promise.resolve(parsed_sequence).then(checkRoleAccess(event.role));
         }
 
-        parsed_sequence.then((sequence) => {
-            flow.args = sequence[1] ? sequence[1].A : undefined;
-            let input = data;
-            let rt_sequence;
-            sequence[0].forEach((handler, index) => {
+        return parsed_sequence.then((sequence) => {
+            event.args = sequence[1].A;
+            let rt_sequence = callHandler(event, sequence[0][0][0], sequence[0][0][1], sequence[0][0][2], input);
+            for (let i = 1; i < sequence[0].length; ++i) {
                 rt_sequence.then((output) => {
                     if (output !== undefined) {
                         input = output;
                     }
-                    callHandler(flow, sequence[0][i][0], sequence[0][i][1], sequence[0][i][2], input);
+                    callHandler(event, sequence[0][i][0], sequence[0][i][1], sequence[0][i][2], input);
                 });
-            });
+            };
 
             if (sequence[1] && sequence[1].E) {
                 handler_p.catch((err) => {
@@ -46,7 +42,7 @@ module.exports = (adapter) => {
 
         }).catch((err) => {
             if (typeof adapter.error === "function") {
-                adapter.error(options.sequence, err);
+                adapter.error(event.sequence, err);
             }
             return Promise.reject(err);
         });
@@ -71,18 +67,18 @@ function parse (adapter, role) {
 
             jobs.push(Fn(adapter, handler[0], role));
 
-            // state
+            // state (memory scope)
             if (handler[1]) {
                 handler[1] = getState(adapter, handler[1]);
             }
 
-            // handler arguments
+            // handler arguments (readonly)
             if (handler[2]) {
                 handler[2] = Object.freeze(handler[2]);
             }
         });
 
-        // sequence arguments
+        // sequence arguments (readonly)
         if (sequence[1] && sequence[1].A) {
             sequence[1].A = Object.freeze(sequence[1].A);
         }
@@ -118,12 +114,8 @@ function getState (adapter, state_id) {
     return state;
 }
 
-function callHandler (flow, handler, state, args, data) {
+function callHandler (event, handler, state, args, input) {
     return new Promise((resolve, reject) => {
-        try {
-            handler(flow, state, args, data, resolve, reject);
-        } catch (err) {
-            reject(err);
-        }
+        handler(event, args, state, input, resolve, reject);
     });
 }
